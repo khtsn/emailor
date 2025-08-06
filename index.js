a5d8fe74eb87cf63c57ec5720d6bc8163cc7957e
KhanhTSN <khanhtsn@gmail.com>
2025-08-07 02:52:58 +0700

    feat: use env port

--- index.js
+++ index.js
@@ -34,6 +34,8 @@ app.get('/api/emails/:from', (req, res) => {
     res.json(sorted);
 });
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
